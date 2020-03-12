010ab199dd7684d884420ce59fd191848fa74270
Alquamah <alqamahjsr01@gmail.com>
2020-03-12 10:08:15 +0530

    8th Commit-User Authentication and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {
-  Fragment
+  Fragment, useEffect
 } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
@@ -11,9 +11,20 @@ import Alert from './components/layout/Alert';
 //Redux
 import {Provider} from 'react-redux';
 import store from './store';
+import {loadUser} from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
-const App = () => ( 
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+const App = () => {
+  useEffect (() => {
+    store.dispatch(loadUser());
+  },[]);
+  
+return(
 <Provider store={store}>
 <Router>
   <Fragment> 
@@ -29,5 +40,5 @@ const App = () => (
   </Fragment > 
 </Router>
 </Provider>
-);
+)};
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React ,{Fragment, useState} from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 import {setAlert} from '../../actions/alert';
+import {register} from '../../actions/auth';
 import PropTypes from 'prop-types'
 
 
-const Register = ({setAlert})=> {
+const Register = ({setAlert, register, isAuthenticated})=> {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -20,10 +21,13 @@ const Register = ({setAlert})=> {
         if(password !== password2) {
             setAlert('Passwords Do Not Match', 'danger')
         } else {
-            console.log('Success');
-        }
+            register({name,email,password});
+    }
     };
 
+    if(isAuthenticated) {
+        return <Redirect to ='/dashboard'></Redirect>
+    }
 
     return (
         <Fragment>
@@ -57,7 +61,7 @@ const Register = ({setAlert})=> {
                     name="password"
                     value={password}
                     onChange={e => onChange(e)}
-                    minLength="6"
+                    minLength='6'
                 />
                 </div>
                 <div className="form-group">
@@ -67,7 +71,7 @@ const Register = ({setAlert})=> {
                     name="password2"
                     value={password2}
                     onChange={e => onChange(e)}
-                    minLength="6"
+                    minLength='6'
                 />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register" />
@@ -77,8 +81,14 @@ const Register = ({setAlert})=> {
             </p>
         </Fragment>
     )
-}
+};
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
-}
-export default connect(null, {setAlert})(Register);
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps, {setAlert, register})(Register);
